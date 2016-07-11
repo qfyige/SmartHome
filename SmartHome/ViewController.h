c766e77a9a6ed598a1ed370e277b2414fa037398
lit <418778315@qq.com>
2016-07-12 07:06:59 +0800

    Initial Commit

---
+++ SmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHome
+//
+//  Created by tong lele on 16/7/12.
+//  Copyright © 2016年 tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
