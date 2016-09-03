c52ecdef3b24cae4e434a764a545b790b72ca1f1
dangeroushuahua <403486864@qq.com>
2016-09-03 14:36:48 +0800

    修改代码冲突

--- SmartHome/Operation/Controller/SHOperationViewController.h
+++ SmartHome/Operation/Controller/SHOperationViewController.h
@@ -8,24 +8,7 @@
 
 #import "SHWebViewController.h"
 
-<<<<<<< Updated upstream
-@interface SHOperationViewController : PAIWebViewController<UIWebViewDelegate,NSURLConnectionDelegate>
-{
-    UIWebView *webView;
-    
-    UIActivityIndicatorView *activityIndicatorView;
-    
-    //当前的url
-    NSURL *_currenURL;
-    NSURLConnection* reUrlConnection;//重发请求
-    NSURLRequest* originRequest;
-    
-}
-
-@property(nonatomic,assign,getter =isAuthed)BOOL authed;
-@property(nonatomic,strong)NSURL *currenURL;
-=======
 @interface SHOperationViewController : SHWebViewController
->>>>>>> Stashed changes
+
 
 @end
